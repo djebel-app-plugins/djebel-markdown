2153654ec5640be9b4acbe4bec0843dcab4a04f7
slavi <admin@orbisius.com>
2025-10-02 18:45:47 +0300

    added processMarkdown and a filter app.content.markdown

--- plugin.php
+++ plugin.php
@@ -17,7 +17,56 @@ text_domain: djebel-markdown
 license: gpl2
 */
 
-if (!class_exists('Djebel_Plugin_Markdown_Shared_Parsedown')) {
-    require_once __DIR__ . '/shared/parsedown/Parsedown.php';
-}
+$obj = Djebel_App_Plugin_Markdown::getInstance();
+Dj_App_Hooks::addFilter('app.content.markdown', [ $obj, 'processMarkdown' ] );
 
+class Djebel_App_Plugin_Markdown {
+    private $parser = null;
+
+    /**
+     * @param string $content
+     * @param array $ctx
+     * @return string
+     * @throws Exception
+     */
+    public function processMarkdown( $content, $ctx = [] ) {
+        // Load on demand
+        if (!class_exists('Djebel_Plugin_Markdown_Shared_Parsedown')) {
+            if (class_exists('Parsedown')) {
+                $this->parser = new Parsedown(); // loaded by the user.
+            } else { // custom class prefixed
+                require_once __DIR__ . '/shared/parsedown/Parsedown.php';
+                $this->parser = new Djebel_Plugin_Markdown_Shared_Parsedown();
+            }
+
+            $this->parser->setSafeMode(true);
+        }
+
+        if (empty($this->parser)) {
+            return $content;
+        }
+
+        $content = Dj_App_Hooks::applyFilter( 'app.plugins.markdown.pre_process', $content, $ctx );
+        $markdown_content = $this->parser->text($content);
+        $markdown_content = Dj_App_Hooks::applyFilter( 'app.plugins.markdown.post_process', $markdown_content, $ctx );
+
+        return $markdown_content;
+    }
+
+    /**
+     * Singleton pattern i.e. we have only one instance of this obj
+     * @staticvar static $instance
+     * @return static
+     */
+    public static function getInstance() {
+        static $instance = null;
+
+        // This will make the calling class to be instantiated.
+        // no need each sub class to define this method.
+        if (is_null($instance)) {
+            $instance = new static();
+        }
+
+        return $instance;
+    }
+}
